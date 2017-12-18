49051667055577ba2e541b4a330097fcbb386765
Wayne Mok <mok@aclanyc.com>
2017-12-18 22:55:39 +0000

    RPS V2

--- js/script.js
+++ js/script.js
@@ -23,8 +23,55 @@
 
 
 //FUNCTIONS
-
+ 
 
 
 // DOCUMENT READY FUNCTION
-
+$(document).ready(function() {
+        var input=$('#userinput');
+        var winner=$('#winner');
+        var user=$('#user');
+        var computer=$('#computer');
+        input.on('keypress',function(event){
+                if(event.keyCode == 13){
+                        checkwinner(input.val().toLowerCase());
+                } 
+        });
+        
+        function checkwinner(userinput) {
+                var choices=['rock','paper','scissors'];
+                var computerchoice=getRandomIntInclusive(0,2);
+                var computerinput=choices[computerchoice];
+                var gamewinner='';
+                console.log(userinput,computerinput);
+                if(userinput == 'rock' && computerinput == 'rock'){
+                        gamewinner='tie';
+                }else if(userinput == 'rock' && computerinput == 'paper'){
+                        gamewinner='computer';
+                }else if(userinput == 'rock' && computerinput == 'scissors'){
+                        gamewinner='user';
+                }else if(userinput == 'paper' && computerinput == 'rock'){
+                        gamewinner='user';
+                }else if(userinput == 'paper' && computerinput == 'paper'){
+                        gamewinner='tie';
+                }else if(userinput == 'paper' && computerinput == 'scissors'){
+                        gamewinner='computer';
+                }else if(userinput == 'scissors' && computerinput == 'rock'){
+                        gamewinner='computer';
+                }else if(userinput == 'scissors' && computerinput == 'paper'){
+                        gamewinner='user';
+                }else if(userinput == 'scissors' && computerinput == 'scissors'){
+                        gamewinner='tie';
+                }
+                winner.html(gamewinner);
+                user.html(userinput);
+                computer.html(computerinput);
+                
+        }
+        //source:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+        function getRandomIntInclusive(min, max) {
+                min = Math.ceil(min);
+                max = Math.floor(max);
+                return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+        }
+});
